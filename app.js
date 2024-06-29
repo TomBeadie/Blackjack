bf15f73b16f5c3d86a67a466445c8eddd1e50177
Tom Beadie <>
2024-06-29 10:16:52 -0400

    Add css, startGame function, declare variables

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('working')
+
+
+let firstCard = 7
+let secondCard = 7
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ''
+
+let messageEl = document.getElementById('message-el')
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Draw a new card?"
+    } else if (sum === 21) {
+        message = "Blackjack!"
+    } else {
+        message = "Lol, loser."
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
